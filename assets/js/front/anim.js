246e404ca86108f7e25105271a2863a66e24428f
Muhammard <jackwilson7268@proton.me>
2024-10-22 16:26:26 +0500

    Front - Add Slider Animation

--- assets/js/front/anim.js
+++ assets/js/front/anim.js
@@ -1,20 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const animScrollOpacities = document.querySelectorAll(".anim-trigger-scroll");
-	const observer = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					entry.target.classList.add("anim-active");
-				} else {
-					// Remove this comment if reaction you want
-					// entry.target.classList.remove("active");
-				}
-			});
-		},
-		{ root: null, rootMargin: "0px", threshold: 0.5 }
-	);
+const scrollAnimation = () => {
+	document.addEventListener("DOMContentLoaded", () => {
+		const animScrollOpacities = document.querySelectorAll(".anim-trigger-scroll");
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						entry.target.classList.add("anim-active");
+					} else {
+					}
+				});
+			},
+			{ root: null, rootMargin: "0px", threshold: 0.5 }
+		);
 
-	Array.from(animScrollOpacities).forEach((elem) => {
-		observer.observe(elem);
+		Array.from(animScrollOpacities).forEach((elem) => {
+			observer.observe(elem);
+		});
 	});
-});
+};
+
+const sliderAnimation = () => {
+	const sliderLists = document.querySelectorAll(".slide-list");
+	let curSlider = 0;
+
+	const slider = () => {
+		for (const sliderList of sliderLists) {
+			const firstChild = sliderList.firstElementChild;
+			if (firstChild) {
+				const childWidth = parseFloat(window.getComputedStyle(firstChild).width);
+				const sliderAmount = childWidth + 8;
+
+				sliderList.style.transform = `translateX(-${sliderAmount * curSlider}px)`;
+			}
+			curSlider = (curSlider + 1) % sliderList.children.length;
+		}
+		console.log(curSlider);
+
+		setTimeout(slider, 5000);
+	};
+
+	document.addEventListener("DOMContentLoaded", slider);
+};
+
+sliderAnimation();
+scrollAnimation();
---
+++ assets/js/style.js
@@ -0,0 +1,22 @@
+function szChild() {
+	const szChildWrappers = document.querySelectorAll(".sz-child");
+	const gap = 8;
+
+	for (const szChildWrapper of szChildWrappers) {
+		szChildWrapper.style.position = "relative";
+		for (let i = 0; i < szChildWrapper.children.length; i++) {
+			const childElem = szChildWrapper.children[i];
+			childElem.style.position = "absolute";
+			childElem.style.top = 0;
+			const childWidth = parseFloat(window.getComputedStyle(childElem).width);
+			childElem.style.left = (childWidth + gap) * i + "px";
+			const parentHeight = parseFloat(window.getComputedStyle(szChildWrapper).height);
+			const parentPadding = parseFloat(window.getComputedStyle(szChildWrapper).paddingTop) + parseFloat(window.getComputedStyle(szChildWrapper).paddingBottom);
+			const childHeight = parseFloat(window.getComputedStyle(childElem).height);
+			console.log(parentHeight, parentPadding, childHeight);
+			if (childHeight + parentPadding > parentHeight) szChildWrapper.style.height = childHeight + parentPadding + "px";
+		}
+	}
+}
+
+szChild();
