41ae15ac3fa05289842daf8b582e50695ce882af
Muhammard <jackwilson7268@proton.me>
2024-10-22 13:08:27 +0500

    Front - Add Animation to Title & Login

---
+++ assets/js/front/anim.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const animScrollOpacities = document.querySelectorAll(".anim-trigger-scroll");
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("anim-active");
+				} else {
+					// Remove this comment if reaction you want
+					// entry.target.classList.remove("active");
+				}
+			});
+		},
+		{ root: null, rootMargin: "0px", threshold: 0.2 }
+	);
+
+	Array.from(animScrollOpacities).forEach((elem) => {
+		observer.observe(elem);
+	});
+});
